ff9850be94985a55082305a862e7a86af770bd90
NTurchi <nicolas@fressets.com>
2020-02-11 11:14:08 +0900

    Initialization details - Returns more details if the connection to the eventstore failed

--- src/event-store/event-store.class.ts
+++ src/event-store/event-store.class.ts
@@ -21,17 +21,26 @@ export class EventStore {
   }
 
   async connect() {
-    this.connection = createConnection(this.settings, this.endpoint);
-    this.connection.connect();
-    this.connection.on('connected', () => {
-      this.logger.log('Connection to EventStore established!');
-      this.isConnected = true;
-    });
-    this.connection.on('closed', () => {
-      this.logger.error('Connection to EventStore closed!');
-      this.isConnected = false;
-      this.connect();
-    });
+    try {
+      this.connection = createConnection(this.settings, this.endpoint);
+      this.connection.connect();
+      this.connection.on('connected', () => {
+        this.logger.log('Connection to EventStore established!');
+        this.isConnected = true;
+      });
+      this.connection.on('closed', () => {
+        this.logger.error('Connection to EventStore closed!');
+        this.isConnected = false;
+        this.connect();
+      });
+    } catch (e) {
+      this.logger.error(
+        `Connection to the event store failed using { connectionSettings: ${JSON.stringify(
+          this.settings,
+        )}, endpoint: ${JSON.stringify(this.endpoint)} }`,
+      );
+      throw e;
+    }
   }
 
   close() {
