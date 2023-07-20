82c0410d7435b318b01d0c237e8e8b8c6a6e7f34
0xb4lamx <medghaith.sellami@gmail.com>
2023-07-20 20:29:52 +0100

    upgrade to Nestjs v10

--- src/event-store/eventstore-cqrs/event-publisher.ts
+++ src/event-store/eventstore-cqrs/event-publisher.ts
@@ -11,7 +11,7 @@ export interface Constructor<T> {
 export class EventPublisher {
   constructor(private readonly eventBus: EventBusProvider) { }
 
-  mergeClassContext<T extends Constructor<AggregateRoot>>(metatype: T): T {
+  mergeClassContext<T extends Constructor<AggregateRoot<IAggregateEvent>>>(metatype: T): T {
     const eventBus = this.eventBus;
     return class extends metatype {
       publish(event: IEvent) {
@@ -20,7 +20,7 @@ export class EventPublisher {
     };
   }
 
-  mergeObjectContext<T extends AggregateRoot>(object: T): T {
+  mergeObjectContext<T extends AggregateRoot<IAggregateEvent>>(object: T): T {
     const eventBus = this.eventBus;
     object.publish = (event: IEvent) => {
       eventBus.publish(event, (event as IAggregateEvent).streamName);
